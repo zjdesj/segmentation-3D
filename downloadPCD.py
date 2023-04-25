1c9383a708267d6e0b6acdbface1022b38f79b4e
王耀武 <wangyaowu@digitalpredict.cn>
2023-04-25 11:42:34 +0200

    update

--- downloadPCD.py
+++ downloadPCD.py
@@ -2,7 +2,8 @@ import subprocess
 import os
 
 __PCD__ = "https://filetransfer.io/data-package/aXA3Sv6d/download"
-subprocess.call('wget ' + __PCD__ + ' -O /content/1.zip')
+subprocess.call('wget ' + __PCD__ + ' -O /content/1.zip', shell=True)
 
-subprocess.call('mkdir /content/pcd')
-subprocess.call('unzip /content/1.zip -d /content/pcd')
+subprocess.call('mkdir /content/pcd', shell=True)
+subprocess.call('unzip /content/1.zip -d /content/pcd', shell=True)
+subprocess.call('rm /content/1.zip', shell=True)
