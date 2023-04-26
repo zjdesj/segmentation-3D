6ad5381218828c7deff7e9a1d15ab1860b39a70e
王耀武 <wangyaowu@digitalpredict.cn>
2023-04-26 10:53:13 +0200

    update

--- utils/downloadPC.py
+++ utils/downloadPC.py
@@ -7,3 +7,10 @@ subprocess.call('wget ' + __PCD__ + ' -O /content/1.zip', shell=True)
 subprocess.call('mkdir /content/pcd', shell=True)
 subprocess.call('unzip /content/1.zip -d /content/pcd', shell=True)
 subprocess.call('rm /content/1.zip', shell=True)
+
+__LAS__ = "https://filetransfer.io/data-package/qVimfcn2/download"
+subprocess.call('wget ' + __LAS__ + ' -O /content/2.zip', shell=True)
+
+subprocess.call('mkdir /content/las', shell=True)
+subprocess.call('unzip /content/2.zip -d /content/las', shell=True)
+subprocess.call('rm /content/2.zip', shell=True)
